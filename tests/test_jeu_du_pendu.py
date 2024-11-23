83ad41fcadd159911cc15374457159e3b34e056c
AntoG24 <Antonin.GARDAIS@estaca.eu>
2024-11-23 17:32:09 +0100

    Create test_jeu_du_pendu.py

---
+++ tests/test_jeu_du_pendu.py
@@ -0,0 +1,18 @@
+# tests/test_hangman.py
+
+# Test d'un exemple de fonction dans hangman.py
+
+# Exemple d'une fonction que tu pourrais avoir dans hangman.py
+def test_check_letter_in_word():
+    from src.hangman import check_letter_in_word
+
+    word = "pendu"
+    letter = "p"
+    result = check_letter_in_word(word, letter)
+
+    assert result == True  # On attend True car "p" est dans "pendu"
+
+    letter = "z"
+    result = check_letter_in_word(word, letter)
+
+    assert result == False  # On attend False car "z" n'est pas dans "pendu"
