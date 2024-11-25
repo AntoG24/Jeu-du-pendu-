679bfc68687a9af492801a602fed9648902ee1a8
AntoG24 <Antonin.GARDAIS@estaca.eu>
2024-11-25 21:15:15 +0100

    Test

--- tests/test_jeu_du_pendu.py
+++ tests/test_jeu_du_pendu.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 
-# Ajouter le dossier 'src' au chemin de recherche des modules
+# Ajouter le dossier 'src' au chemin de recherche des modules.
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
 )
